6c4887af83749cb148f966ee3e4bd47768a22acb
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-10-03 16:51:51 +0530

    Apollo config

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import "./App.css";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 
 function App() {
+  const client = new ApolloClient({
+    uri: "https://countries.trevorblades.com/",
+    cache: new InMemoryCache(),
+  });
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
-      </Router>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+          </Routes>
+        </Router>
+      </div>
+    </ApolloProvider>
   );
 }
 

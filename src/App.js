fd6c0e94f29f8136dbcefc2b37b4806e15502990
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-10-03 16:26:29 +0530

    Routes set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

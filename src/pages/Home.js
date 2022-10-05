8dfaff3b966c35394dd28a94d2ee33b92d559ffb
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-10-05 13:01:39 +0530

    Retrieving countries

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,40 @@
+import { gql, useQuery } from "@apollo/client";
+
+const LIST_OF_COUNTRIES = gql`
+  {
+    countries {
+      name
+      capital
+      code
+      emoji
+    }
+  }
+`;
+
 function Home() {
-  return <div>Home</div>;
+  const { loading, error, data } = useQuery(LIST_OF_COUNTRIES);
+
+  return (
+    <div className="home">
+      <h1>List of Countries</h1>
+      <div className="listOfCountries">
+        {loading && <h3>Loading...</h3>}
+        {error && <h3>error.message</h3>}
+        {data &&
+          data.countries.map((country) => (
+            <div key={country.code} className="country">
+              <h2>
+                {country.name}
+                <label>{country.emoji}</label>
+              </h2>
+              <h4>
+                {country.capital} | {country.code}
+              </h4>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
